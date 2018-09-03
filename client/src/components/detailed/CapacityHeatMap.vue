198891903297309f83001ca42526a6a62da1dc96
vinaymor002 <vinaymor002@gmail.com>
2018-09-03 12:18:41 +0530

    XOL-4113: Added patterns

--- client/src/components/detailed/CapacityHeatMap.vue
+++ client/src/components/detailed/CapacityHeatMap.vue
@@ -2,12 +2,32 @@
     <div class="chart-container" ref="chartContainer">
         <svg style="height: 0">
             <defs>
-                <pattern id="zeroed-out" width="8" height="8" patternUnits="userSpaceOnUse"
-                         patternTransform="rotate(45)">
-                    <rect width="8" height="8" transform="translate(7,0)" fill="black"></rect>
+                <pattern id="zeroed-out" width="100%" height="100%" patternContentUnits="objectBoundingBox">
+                    <polyline points="0 1, 1 0" style="fill:none;stroke:black;stroke-width:.03"/>
                 </pattern>
 
                 <pattern id="private" width="1" height="1" patternContentUnits="objectBoundingBox">
+                    <polygon points=".5 0,1 0,1 .5" fill="blue"/>
+                </pattern>
+
+                <pattern id="private-zeroed-out" width="100%" height="100%" patternContentUnits="objectBoundingBox">
+                    <polyline points="0 1, 1 0" style="fill:none;stroke:black;stroke-width:.03"/>
+                    <polygon points=".5 0,1 0,1 .5" fill="blue"/>
+                </pattern>
+
+                <pattern id="private-closed" width="100%" height="100%" patternContentUnits="objectBoundingBox">
+                    <polygon points="0 0, .5 0, 1 .5, 1 1, 0 1" fill="#c7c7c7"/>
+                    <polygon points=".5 0,1 0,1 .5" fill="blue"/>
+                </pattern>
+
+                <pattern id="private-overbooked" width="100%" height="100%" patternContentUnits="objectBoundingBox">
+                    <polygon points="0 0, .5 0, 1 .5, 1 1, 0 1" fill="#ff0000"/>
+                    <polygon points=".5 0,1 0,1 .5" fill="blue"/>
+                </pattern>
+
+                <pattern id="private-inventory-restriction" width="100%" height="100%"
+                         patternContentUnits="objectBoundingBox">
+                    <polygon points="0 0, .5 0, 1 .5, 1 1, 0 1" fill="orange"/>
                     <polygon points=".5 0,1 0,1 .5" fill="blue"/>
                 </pattern>
             </defs>
@@ -43,13 +63,21 @@
             </div>
 
             <div class="legend">
-                <svg class="private">
+                <svg class="legend-private">
                     <rect height="30px" width="30px" class="private"></rect>
                 </svg>
                 <label>Private</label>
             </div>
 
-
+            <div class="legend">
+                <svg class="legend-manually-adjusted">
+                    <rect stroke="#DC5D42" fill="white" x="0" y="0" width="30" height="30"></rect>
+                    <text fill="blue">
+                        <tspan x="5" y="22">10</tspan>
+                    </text>
+                </svg>
+                <label>Manually adjusted</label>
+            </div>
         </div>
 
         <highcharts class="chart" :options="options" :updateArgs=[true]></highcharts>
@@ -129,24 +157,24 @@
             let manuallyAdjusted = slotsCapacity[slotTime].capcityManuallyAdjusted;
             let closed = slotsCapacity[slotTime].closed;
             let overbooked = capacity < reserved;
-            let invantoryRestriction = false;
+            let inventoryRestriction = false;
 
             if (capacity === 0) {
               if (manuallyAdjusted) {
                 className = 'manual-zeroed-out'
               } else {
                 className = ' inventory-restriction'
-                invantoryRestriction = true
+                inventoryRestriction = true
               }
             } else {
               occupiedPercentage = parseFloat((reserved / capacity * 100).toFixed(0));
             }
 
             if (privateEvent) {
-              className += ' private_event'
+              className += ' private'
             }
             if (manuallyAdjusted) {
-              className += ' manually_adjusted'
+              className += ' manually-adjusted'
             }
 
             if (overbooked) {
@@ -160,6 +188,9 @@
             seriesData.push({
               x: dateIndex,
               y: slotIndex,
+              dataLabels: {
+                className: className
+              },
               start: start,
               value: occupiedPercentage,
               className: className,
@@ -169,7 +200,7 @@
               manuallyAdjusted: manuallyAdjusted,
               overbooked: overbooked,
               closed: closed,
-              inventoryRestriction: invantoryRestriction
+              inventoryRestriction: inventoryRestriction
             });
           }
         }
@@ -372,6 +403,26 @@
 
     .legend > svg > rect {
         stroke: #d5ff9e
+    }
+
+    .private.manual-zeroed-out {
+        fill: url(#private-zeroed-out)
+    }
+
+    .private.inventory-restriction {
+        fill: url(#private-inventory-restriction)
+    }
+
+    .private.closed {
+        fill: url(#private-closed)
+    }
+
+    .private.overbooked {
+        fill: url(#private-overbooked)
+    }
+
+    .manually-adjusted > text {
+        fill: blue !important;
     }
 </style>
 
